44cb9d2e2ade42ff2cb905b61cc55bf199e3bf8f
REDGAMERGO <str1ngkill@yandex.ru>
2021-02-18 20:57:07 +0300

    Вывод товара в виде плиток на html-странице

--- src/main/java/org/reallume/controller/MainController.java
+++ src/main/java/org/reallume/controller/MainController.java
@@ -1,12 +1,14 @@
 package org.reallume.controller;
 
-import org.reallume.model.extra.Bacon;
-import org.reallume.model.extra.Chicken;
+import org.reallume.model.extra.*;
 import org.reallume.model.pizza.BasePizza;
 import org.reallume.model.pizza.Pizza;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 
@@ -16,13 +18,22 @@ public class MainController {
     private static Logger LOGGER = Logger.getLogger("InfoLogging");
 
     @GetMapping(value = "/main")
-    public String mainPage(){
+    public String mainPage(Model model){
+
+        List<Pizza> pizzas = new ArrayList<>();
+        pizzas.add(new BasePizza());
+        pizzas.add(new Bacon(new BasePizza()));
+        pizzas.add(new Chicken(new BasePizza()));
+        pizzas.add(new Chicken(new Tomato(new BasePizza())));
+        pizzas.add(new Chicken(new Tomato(new Mushroom(new Sausage(new BasePizza())))));
+
+        model.addAttribute("pizzas", pizzas);
 
         Pizza pizza = new Bacon(new BasePizza());
         LOGGER.info(pizza.getName() + " " + pizza.getPrice());
         pizza = new Chicken(new BasePizza());
         LOGGER.info(pizza.getName() + " " + pizza.getPrice());
-        pizza = new Chicken(new Bacon(new BasePizza()));
+        pizza = new Chicken(new Tomato(new BasePizza()));
         LOGGER.info(pizza.getName() + " " + pizza.getPrice());
 
 
